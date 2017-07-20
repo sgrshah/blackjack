a6ab2508b9b26a054c8082b2ddecc2804d3130c6
Sagar Shah <sgrshah@Sagars-MacBook-Pro.local>
2017-07-20 16:54:46 -0400

    WIP add TestType to simulation. - Spec out split and double down methods. - Introduce TestType parameter to specify what action should be taken. - Pass TestType around and amend existing method's arities. - Incorporate double down into game. - Calculate expected value in game:play.

--- src/actions.erl
+++ src/actions.erl
@@ -1,21 +1,26 @@
 -module(actions).
--export([decide/3]).
+-export([decide/3, decide/4]).
 
-decide(Shoe, player, {player, PlayerHand, dealer, DealerHand}) ->
+decide(Shoe, player, {player, PlayerHand, dealer, DealerHand}, TestType) ->
   PlayerCount = hand:sum(PlayerHand),
   io:format("PlayerCount:~p~n", [PlayerCount]),
 
   if
     PlayerCount < 17 ->
-      io:format("player decides to hit~n"),
-      hit(Shoe, player, {player, PlayerHand, dealer, DealerHand});
+      io:format("TestType:~p~n", [TestType]),
+      case TestType of
+        hit -> hit(Shoe, player, {player, PlayerHand, dealer, DealerHand}, TestType);
+        double -> double(Shoe, player, {player, PlayerHand, dealer, DealerHand});
+        %% split -> split(Shoe, player, {player, PlayerHand, dealer, DealerHand});
+        stand -> game:play(Shoe, dealer, {player, PlayerHand, dealer, DealerHand})
+      end;
     PlayerCount > 21 ->
       io:format("player busts~n"),
       {player, PlayerHand, dealer, DealerHand};
     (PlayerCount >= 17) and (PlayerCount =< 21) ->
       io:format("player decides to stay.~n"),
       game:play(Shoe, dealer, {player, PlayerHand, dealer, DealerHand})
-  end;
+  end.
 
 decide(Shoe, dealer, {player, PlayerHand, dealer, DealerHand}) ->
   DealerCount = hand:sum(DealerHand),
@@ -23,7 +28,6 @@ decide(Shoe, dealer, {player, PlayerHand, dealer, DealerHand}) ->
 
   if
     DealerCount < 17 ->
-      io:format("dealer decides to hit~n"),
       hit(Shoe, dealer, {player, PlayerHand, dealer, DealerHand});
     DealerCount > 21 ->
       io:format("dealer busts~n"),
@@ -33,11 +37,25 @@ decide(Shoe, dealer, {player, PlayerHand, dealer, DealerHand}) ->
       {player, PlayerHand, dealer, DealerHand}
   end.
 
-hit(Shoe, player, {player, PlayerHand, dealer, DealerHand}) ->
+hit(Shoe, player, {player, PlayerHand, dealer, DealerHand}, TestType) ->
+  io:format("player decides to hit~n"),
   CardNumInShoe = length(PlayerHand) + length(DealerHand) + 1,
   NewGameState = {player, PlayerHand ++ [shoe:deal(Shoe, CardNumInShoe)], dealer, DealerHand},
-  decide(Shoe, player, NewGameState);
+  decide(Shoe, player, NewGameState, TestType).
 hit(Shoe, dealer, {player, PlayerHand, dealer, DealerHand}) ->
+  io:format("dealer decides to hit~n"),
   CardNumInShoe = length(PlayerHand) + length(DealerHand) + 1,
   NewGameState = {player, PlayerHand, dealer, DealerHand ++ [shoe:deal(Shoe, CardNumInShoe)]},
   decide(Shoe, dealer, NewGameState).
+
+double(Shoe, player, {player, PlayerHand, dealer, DealerHand}) ->
+  io:format("player decides to double~n"),
+  CardNumInShoe = length(PlayerHand) + length(DealerHand) + 1,
+  NewGameState = {player, PlayerHand ++ [shoe:deal(Shoe, CardNumInShoe)], dealer, DealerHand},
+  game:play(Shoe, dealer, NewGameState).
+
+%% split(Shoe, player, {player, [S,S], dealer, DealerHand}) ->
+%%   io:format("split~p~n", [S]),
+%%   io:format("player decides to split.~n");
+%% split(_,_,_) ->
+%%   io:format("can't split~n").
--- src/game.erl
+++ src/game.erl
@@ -1,39 +1,56 @@
 -module(game).
--export([play/0, play/3]).
+-export([play/1, play/3, play/4]).
 
 % This module encapsulates all high level game logic
+%
+% Hardcoded rules:
+% Dealer stands on soft 17.
+% 1 deck
+% No splits
 
-play() ->
+play(TestType) ->
   Shoe = shoe:create({decks, 1}),
   io:format("~p~n", [Shoe]),
   InitialHands = dealer:deal(Shoe),
-  Result = play(Shoe, player, InitialHands),
-  io:format("Result~p~n", [Result]).
+  Result = play(Shoe, player, InitialHands, TestType),
+  Response = [Result, determine_expected_value(Result)],
+  io:format("Result~w~n", [Response]).
 
-play(Shoe, player, {player, PlayerHand, dealer, DealerHand}) ->
+play(Shoe, player, {player, PlayerHand, dealer, DealerHand}, TestType) ->
   io:format("player's turn~n"),
   PlayerCount = hand:sum(PlayerHand),
-  io:format("PlayerCount:~p~n", [PlayerCount]),
 
   if
     PlayerCount == 21 ->
+      io:format("PlayerCount:~p~n", [PlayerCount]),
       io:format("player wins blackjack~n"),
       {player, PlayerHand, dealer, DealerHand};
     PlayerCount < 21 ->
-      actions:decide(Shoe, player, {player, PlayerHand, dealer, DealerHand})
-  end;
+      actions:decide(Shoe, player, {player, PlayerHand, dealer, DealerHand}, TestType)
+  end.
 
 play(Shoe, dealer, {player, PlayerHand, dealer, DealerHand}) ->
   io:format("dealer's turn~n"),
 
   DealerCount = hand:sum(DealerHand),
-  io:format("DealerCount:~p~n", [DealerCount]),
 
   if
     DealerCount == 21 ->
+      io:format("DealerCount:~p~n", [DealerCount]),
       io:format("dealer has blackjack~n"),
       {player, PlayerHand, dealer, DealerHand};
     DealerCount < 21 ->
       actions:decide(Shoe, dealer, {player, PlayerHand, dealer, DealerHand})
   end.
 
+determine_expected_value({player, PlayerHand, dealer, DealerHand}) ->
+  PlayerCount = hand:sum(PlayerHand),
+  DealerCount = hand:sum(DealerHand),
+
+  if
+    PlayerCount > 21 -> {expected_value, -1};
+    DealerCount > 21 -> {expected_value, 1};
+    PlayerCount > DealerCount -> {expected_value, 1};
+    PlayerCount ==  DealerCount -> {expected_value, 0};
+    PlayerCount <  DealerCount -> {expected_value, -1}
+  end.
